c7002f3646f0638b26856c4930fd316b94a8c68e
Jack Kosaian <jackkosaian@gmail.com>
2017-02-09 17:45:47 -0500

    IMPL : Add APSP to NetGraph

--- src/main/java/gaiasim/network/NetGraph.java
+++ src/main/java/gaiasim/network/NetGraph.java
@@ -1,15 +1,22 @@
 package gaiasim.network;
 
+import org.graphstream.algorithm.APSP;
+import org.graphstream.algorithm.APSP.APSPInfo;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class NetGraph {
     public Graph graph_;
     public ArrayList<String> nodes_ = new ArrayList<String>();
+
+    // All pairs shortest path. First index is src node, second index
+    // is dst node.
+    public Path[][] apsp_;
 
     public NetGraph(String gml_file) throws java.io.IOException {
         graph_ = new SingleGraph("GaiaSimGraph");
@@ -21,7 +28,27 @@ public class NetGraph {
         fs.removeSink(graph_);
 
         for (Node n:graph_) {
-            nodes_.add(n.getLabel("ui.label").toString());
+            String id = n.getLabel("ui.label").toString();
+            nodes_.add(id);
+        }
+        
+        APSP apsp = new APSP();
+        apsp.init(graph_);
+        apsp.setDirected(false);
+        apsp.compute();
+       
+        // Since we'll be indexing this array by nodeID, and nodeID's start
+        // at 1, we need num_nodes+1 entries in the array.
+        apsp_ = new Path[nodes_.size() + 1][nodes_.size() + 1];
+        for (Node n : graph_) {
+            APSPInfo info = n.getAttribute(APSPInfo.ATTRIBUTE_NAME);
+
+            for (Node n_ : graph_) {
+                if (n.toString() != n_.toString()) {
+                    apsp_[Integer.parseInt(n.toString())][Integer.parseInt(n_.toString())] 
+                        = info.getShortestPathTo(n_.toString());
+                }
+            }
         }
     }
 }
