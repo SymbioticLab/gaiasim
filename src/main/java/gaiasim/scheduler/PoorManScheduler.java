424f024ced17d0c2711d9831795821da24ed0422
Jack Kosaian <jackkosaian@gmail.com>
2017-02-17 14:22:25 -0500

    CLEAN : Refactor ridiculously large schedule_flows function

--- src/main/java/gaiasim/scheduler/PoorManScheduler.java
+++ src/main/java/gaiasim/scheduler/PoorManScheduler.java
@@ -38,9 +38,191 @@ public class PoorManScheduler extends Scheduler {
         }
     }
 
+    public void make_paths(Flow f, ArrayList<Link> link_vals) {
+        // This portion is similar to Flow::find_pathway_with_link_allocation in Sim
+        ArrayList<Pathway> potential_paths = new ArrayList<Pathway>();
+        ArrayList<Pathway> completed_paths = new ArrayList<Pathway>();
+
+        // Find all links in the network from the flow's source that have some bandwidth
+        // availible and start paths from them.
+        ArrayList<Link> links_to_remove = new ArrayList<Link>();
+        for (Link l : link_vals) {
+            if (l.src_loc_.equals(f.src_loc_)) {
+                Pathway p = new Pathway();
+                p.node_list_.add(l.src_loc_);
+                p.node_list_.add(l.dst_loc_);
+                p.bandwidth_ = l.cur_bw_;
+
+                if (l.dst_loc_.equals(f.dst_loc_)) {
+                    completed_paths.add(p);
+                }
+                else {
+                    potential_paths.add(p);
+                }
+
+                links_to_remove.add(l);
+            }
+        }
+
+        // Remove any Links that were added above
+        for (Link l : links_to_remove) {
+            link_vals.remove(l);
+        }
+
+        // Iterate through remaining links and try to add them to paths
+        ArrayList<Pathway> paths_to_remove = new ArrayList<Pathway>();
+        boolean link_added;
+        while (!link_vals.isEmpty()) {
+            links_to_remove.clear();
+            link_added = false;
+
+            for (Link l : link_vals) {
+                if (l.cur_bw_ == 0.0) {
+                    links_to_remove.add(l);
+                    continue;
+                }
+
+                for (Pathway p : potential_paths) {
+                    // Does this link fit after the current last node in the path?
+                    if (!p.last_node().equals(l.src_loc_)) {
+                        continue;
+                    }
+
+                    // Does the bandwidth available on this link directly match the bandwidth
+                    // of this pathway?
+                    if (Math.round(Math.abs(p.bandwidth_ - l.cur_bw_) * 100.0) / 100.0 < 0.01) {
+                        p.node_list_.add(l.dst_loc_);
+                        links_to_remove.add(l);
+                        link_added = true;
+                        links_to_remove.add(l);
+
+                        // Check if path is now complete
+                        if (l.dst_loc_.equals(f.dst_loc_)) {
+                            paths_to_remove.add(p);
+                            completed_paths.add(p);
+                        }
+                    }
+
+                    // Does this link have less bandwidth than the bandwidth available on the path?
+                    // Split the path in two -- one path taking this link (and reducing its bandwidth)
+                    // and the other not taking the path and using the remaining bandwidth.
+                    else if (Math.round((p.bandwidth_ - l.cur_bw_ * 100.0)) / 100.0 >= 0.01) {
+                        Pathway new_p = new Pathway();
+                        new_p.bandwidth_ = p.bandwidth_ - l.cur_bw_;
+                        new_p.node_list_ = (ArrayList<String>)p.node_list_.clone();
+                        p.bandwidth_ = l.cur_bw_;
+                        p.node_list_.add(l.dst_loc_);
+                        link_added = true;
+                        links_to_remove.add(l);
+
+                        // Check if path is now complete
+                        if (l.dst_loc_.equals(f.dst_loc_)) {
+                            paths_to_remove.add(p);
+                            completed_paths.add(p);
+                        }
+                    }
+
+                    // Does the link have more bandwidth than the bandwidth available on the path?
+                    // Only reduce the link's bandwidth by the amount that could be used by the path.
+                    else if (Math.round((p.bandwidth_ - l.cur_bw_ * 100.0)) / 100.0 <= -0.01) {
+                        l.cur_bw_ = l.cur_bw_ - p.bandwidth_;
+                        p.node_list_.add(l.dst_loc_);
+                        link_added = true;
+                        // Check if path is now complete
+                        if (l.dst_loc_.equals(f.dst_loc_)) {
+                            paths_to_remove.add(p);
+                            completed_paths.add(p);
+                        }
+                    }
+
+                } // for pathway
+
+                // Remove any paths that have been completed during this last round
+                for (Pathway p : paths_to_remove) {
+                    potential_paths.remove(p);
+                }
+
+            } // for link
+
+            // Remove any Links that were added above
+            for (Link l : links_to_remove) {
+                link_vals.remove(l);
+            }
+
+            // If we were unable to add any links this round, just quit
+            if (!link_added) {
+                break;
+            }
+        } // while link_vals
+        f.paths_.clear();
+        f.paths_ = completed_paths;
+    }
+
     public void progress_flow(Flow f) {
         for (Pathway p : f.paths_) {
             f.transmitted_ += p.bandwidth_ * Constants.SIMULATION_TIMESTEP_SEC;
+        }
+    }
+
+    public double remaining_bw() {
+        double remaining_bw = 0.0;
+        for (int i = 0; i < net_graph_.nodes_.size() + 1; i++) {
+            for (int j = 0; j < net_graph_.nodes_.size() + 1; j++) {
+                if (links_[i][j] != null) {
+                    remaining_bw += links_[i][j].remaining_bw();
+                }
+            }
+        }
+
+        return remaining_bw;
+    }
+
+    public void schedule_extra_flows(ArrayList<Coflow> unscheduled_coflows, long timestamp) {
+        ArrayList<Flow> unscheduled_flows = new ArrayList<Flow>();
+        for (Coflow c : unscheduled_coflows) {
+            for (String k : c.flows_.keySet()) {
+                unscheduled_flows.add(c.flows_.get(k));
+            }
+        }
+        Collections.sort(unscheduled_flows, new Comparator<Flow>() {
+            public int compare(Flow o1, Flow o2) {
+                if (o1.volume_ == o2.volume_) return 0;
+                return o1.volume_ < o2.volume_ ? -1 : 1;
+            }
+        });
+
+        for (Flow f : unscheduled_flows) {
+            int src = Integer.parseInt(f.src_loc_);
+            int dst = Integer.parseInt(f.dst_loc_);
+            Pathway p = new Pathway(net_graph_.apsp_[src][dst]);
+
+            double min_bw = Double.MAX_VALUE;
+            SubscribedLink[] path_links = new SubscribedLink[p.node_list_.size() - 1];
+            for (int i = 0; i < p.node_list_.size() - 1; i++) {
+                int lsrc = Integer.parseInt(p.node_list_.get(i));
+                int ldst = Integer.parseInt(p.node_list_.get(i+1));
+                SubscribedLink l = links_[lsrc][ldst];
+
+                double bw = l.remaining_bw();
+                path_links[i] = l;
+                if (bw < min_bw) {
+                    min_bw = bw;
+                }
+            }
+
+            if (min_bw > 0) {
+                for (SubscribedLink l : path_links) {
+                    l.subscribers_.add(p);
+                }
+                f.paths_.clear();
+                f.paths_.add(p);
+
+                System.out.println("Adding separate flow " + f.id_);
+                if (f.start_timestamp_ == -1) {
+                    f.start_timestamp_ = timestamp;
+                }
+                flows_.put(f.id_, f);
+            }
         }
     }
 
@@ -74,125 +256,8 @@ public class PoorManScheduler extends Scheduler {
                 assert(link_vals != null);
 
                 // This portion is similar to Flow::make() in Sim
-
-                // This portion is similar to Flow::find_pathway_with_link_allocation in Sim
-                ArrayList<Pathway> potential_paths = new ArrayList<Pathway>();
-                ArrayList<Pathway> completed_paths = new ArrayList<Pathway>();
-
-                // Find all links in the network from the flow's source that have some bandwidth
-                // availible and start paths from them.
-                ArrayList<Link> links_to_remove = new ArrayList<Link>();
-                for (Link l : link_vals) {
-                    if (l.src_loc_.equals(f.src_loc_)) {
-                        Pathway p = new Pathway();
-                        p.node_list_.add(l.src_loc_);
-                        p.node_list_.add(l.dst_loc_);
-                        p.bandwidth_ = l.cur_bw_;
-
-                        if (l.dst_loc_.equals(f.dst_loc_)) {
-                            completed_paths.add(p);
-                        }
-                        else {
-                            potential_paths.add(p);
-                        }
-                        
-                        links_to_remove.add(l);
-                    }
-                }
-
-                // Remove any Links that were added above
-                for (Link l : links_to_remove) {
-                    link_vals.remove(l);
-                }
+                make_paths(f, link_vals);
                 
-                // Iterate through remaining links and try to add them to paths
-                ArrayList<Pathway> paths_to_remove = new ArrayList<Pathway>();
-                boolean link_added;
-                while (!link_vals.isEmpty()) {
-                    links_to_remove.clear();
-                    link_added = false;
-
-                    for (Link l : link_vals) {
-                        if (l.cur_bw_ == 0.0) {
-                            links_to_remove.add(l);
-                            continue;
-                        }
-
-                        for (Pathway p : potential_paths) {
-                            // Does this link fit after the current last node in the path?
-                            if (!p.last_node().equals(l.src_loc_)) {
-                                continue;
-                            }
-
-                            // Does the bandwidth available on this link directly match the bandwidth
-                            // of this pathway?
-                            if (Math.round(Math.abs(p.bandwidth_ - l.cur_bw_) * 100.0) / 100.0 < 0.01) {
-                                p.node_list_.add(l.dst_loc_);
-                                links_to_remove.add(l);
-                                link_added = true;
-                                links_to_remove.add(l);
-
-                                // Check if path is now complete
-                                if (l.dst_loc_.equals(f.dst_loc_)) {
-                                    paths_to_remove.add(p);
-                                    completed_paths.add(p);
-                                }
-                            }
-
-                            // Does this link have less bandwidth than the bandwidth available on the path?
-                            // Split the path in two -- one path taking this link (and reducing its bandwidth)
-                            // and the other not taking the path and using the remaining bandwidth.
-                            else if (Math.round((p.bandwidth_ - l.cur_bw_ * 100.0)) / 100.0 >= 0.01) {
-                                Pathway new_p = new Pathway();
-                                new_p.bandwidth_ = p.bandwidth_ - l.cur_bw_;
-                                new_p.node_list_ = (ArrayList<String>)p.node_list_.clone();
-                                p.bandwidth_ = l.cur_bw_;
-                                p.node_list_.add(l.dst_loc_);
-                                link_added = true;
-                                links_to_remove.add(l);
-                                
-                                // Check if path is now complete
-                                if (l.dst_loc_.equals(f.dst_loc_)) {
-                                    paths_to_remove.add(p);
-                                    completed_paths.add(p);
-                                }
-                            }
-
-                            // Does the link have more bandwidth than the bandwidth available on the path?
-                            // Only reduce the link's bandwidth by the amount that could be used by the path.
-                            else if (Math.round((p.bandwidth_ - l.cur_bw_ * 100.0)) / 100.0 <= -0.01) {
-                                l.cur_bw_ = l.cur_bw_ - p.bandwidth_;
-                                p.node_list_.add(l.dst_loc_);
-                                link_added = true;
-                                // Check if path is now complete
-                                if (l.dst_loc_.equals(f.dst_loc_)) {
-                                    paths_to_remove.add(p);
-                                    completed_paths.add(p);
-                                }
-                            }
-
-                        } // for pathway
-
-                        // Remove any paths that have been completed during this last round
-                        for (Pathway p : paths_to_remove) {
-                            potential_paths.remove(p);
-                        }
-
-                    } // for link
-                    
-                    // Remove any Links that were added above
-                    for (Link l : links_to_remove) {
-                        link_vals.remove(l);
-                    }
-
-                    // If we were unable to add any links this round, just quit
-                    if (!link_added) {
-                        break;
-                    }
-                } // while link_vals
-                f.paths_.clear();
-                f.paths_ = completed_paths;
-
                 // Subscribe the flow's paths to the links it uses
                 for (Pathway p : f.paths_) {
                     for (int i = 0; i < p.node_list_.size() - 1; i++) {
@@ -219,67 +284,9 @@ public class PoorManScheduler extends Scheduler {
 
         // Schedule any available flows
         if (!unscheduled_coflows.isEmpty() && remaining_bw() > 0.0) {
-            ArrayList<Flow> unscheduled_flows = new ArrayList<Flow>();
-            for (Coflow c : unscheduled_coflows) {
-                for (String k : c.flows_.keySet()) {
-                    unscheduled_flows.add(c.flows_.get(k));
-                }
-            }
-            Collections.sort(unscheduled_flows, new Comparator<Flow>() {
-                public int compare(Flow o1, Flow o2) {
-                    if (o1.volume_ == o2.volume_) return 0;
-                    return o1.volume_ < o2.volume_ ? -1 : 1;
-                }
-            });
-
-            for (Flow f : unscheduled_flows) {
-                int src = Integer.parseInt(f.src_loc_);
-                int dst = Integer.parseInt(f.dst_loc_);
-                Pathway p = new Pathway(net_graph_.apsp_[src][dst]);
-
-                double min_bw = Double.MAX_VALUE;
-                SubscribedLink[] path_links = new SubscribedLink[p.node_list_.size() - 1];
-                for (int i = 0; i < p.node_list_.size() - 1; i++) {
-                    int lsrc = Integer.parseInt(p.node_list_.get(i));
-                    int ldst = Integer.parseInt(p.node_list_.get(i+1));
-                    SubscribedLink l = links_[lsrc][ldst];
-
-                    double bw = l.remaining_bw();
-                    path_links[i] = l;
-                    if (bw < min_bw) {
-                        min_bw = bw;
-                    }
-                }
-
-                if (min_bw > 0) {
-                    for (SubscribedLink l : path_links) {
-                        l.subscribers_.add(p);
-                    }
-                    f.paths_.clear();
-                    f.paths_.add(p);
-
-                    System.out.println("Adding separate flow " + f.id_);
-                    if (f.start_timestamp_ == -1) {
-                        f.start_timestamp_ = timestamp;
-                    }
-                    flows_.put(f.id_, f);
-                }
-            }
+            schedule_extra_flows(unscheduled_coflows, timestamp);        
         }
         return flows_;
-    }
-
-    public double remaining_bw() {
-        double remaining_bw = 0.0;
-        for (int i = 0; i < net_graph_.nodes_.size() + 1; i++) {
-            for (int j = 0; j < net_graph_.nodes_.size() + 1; j++) {
-                if (links_[i][j] != null) {
-                    remaining_bw += links_[i][j].remaining_bw();
-                }
-            }
-        }
-
-        return remaining_bw;
     }
     
     public ArrayList<Map.Entry<Coflow, Double>> sort_coflows(HashMap<String, Coflow> coflows) throws Exception {
