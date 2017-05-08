b385051784018e2e92a1dd9faa5cf104b6ffd34e
jimmyyou <jieyou@umich.edu>
2017-05-07 21:16:53 -0400

    add utils

---
+++ util/README.md
@@ -0,0 +1,6 @@
+prepare.sh is a simplified install script for use on CloudLab with a snapshot which has prerequisites installed.
+
+To download floodlight and compile both gaiasim and floodlight, run:
+```
+./prepare.sh
+```
---
+++ util/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is used to download and build codes
+# to perform  emulation.
+
+cd ~
+git clone https://github.com/jackkosaian/floodlight.git
+cd floodlight
+git submodule init
+git submodule update
+ant 
+sudo mkdir /var/lib/floodlight
+sudo chmod 777 /var/lib/floodlight
+
+echo "Floodlight ready for use, now compiling gaiasim..."
+
+cd ~/gaiasim
+mvn package
+
+echo "Gaiasim now ready, preparing output folders..."
+./util/make_output_dir.sh
