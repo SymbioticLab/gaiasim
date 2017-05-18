0ca3761a411a47ccd933da785a3ce3c813335216
jimmyyou <jieyou@umich.edu>
2017-05-18 10:47:37 -0400

    updated message format

--- src/main/java/gaiasim/gaiamessage/FlowStatusMessage.java
+++ src/main/java/gaiasim/gaiamessage/FlowStatusMessage.java
@@ -7,16 +7,27 @@ import java.util.HashMap;
 
 public class FlowStatusMessage extends AgentMessage{
 
-    // the payload
-    private HashMap<String , Double> flow_to_progress_map;
+    // the payload, use arrays as the payload
+//    private HashMap<String , Double> flow_to_progress_map;
+    int size;
+    String  [] id;
+    double  [] transmitted;
+    boolean [] isFinished;
 
-    public FlowStatusMessage(HashMap<String, Double> flow_to_progress_map){
+    public FlowStatusMessage(int size, String[] id, double[] transmitted, boolean[] isFinished) {
         this.type = Type.FLOW_STATUS;
-
-        this.flow_to_progress_map = flow_to_progress_map;
-
+        this.size = size;
+        this.id = id;
+        this.transmitted = transmitted;
+        this.isFinished = isFinished;
     }
 
-    public HashMap<String, Double> getFlow_to_progress_map() {    return flow_to_progress_map;    }
+    public FlowStatusMessage(String id , double transmitted , boolean isFinished){
+        this.type = Type.FLOW_STATUS;
+        this.size = 1;
+        this.id = new String[]{id};
+        this.transmitted = new double[]{transmitted};
+        this.isFinished = new boolean[]{isFinished};
+    }
 
 }
