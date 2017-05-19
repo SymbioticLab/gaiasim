0bd6e386adfcbc0d72127dff30f65f40525c24cc
jimmyyou <jieyou@umich.edu>
2017-05-19 14:34:49 -0400

    added port option

--- src/main/java/gaiasim/agent/ReceivingAgent.java
+++ src/main/java/gaiasim/agent/ReceivingAgent.java
@@ -11,6 +11,11 @@ public class ReceivingAgent {
         int port = 33330;
         ServerSocket sd;
 
+        if(args.length == 1){
+            port = Integer.parseInt(args[0]);
+            System.out.println("Running with 1 argument, set port to " + port);
+        }
+
         try {
             sd = new ServerSocket(port);
             while (true) {
