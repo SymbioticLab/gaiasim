523187bbc49c971187ea40729c4d105367860745
jimmyyou <jieyou@umich.edu>
2017-09-05 22:55:46 -0400

    in baseline try match not only TCP packets

--- util/base_script_routing/set_baserules_ny.sh
+++ util/base_script_routing/set_baserules_ny.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo ip netns exec ny iptables -A PREROUTING -t mangle -p tcp -d 10.0.1.1 -j MARK --set-mark 2
-sudo ip netns exec ny iptables -A PREROUTING -t mangle -p tcp -d 10.0.2.2 -j MARK --set-mark 3
-sudo ip netns exec ny iptables -A PREROUTING -t mangle -p tcp -d 10.0.3.3 -j MARK --set-mark 1
-sudo ip netns exec ny iptables -A PREROUTING -t mangle -p tcp -d 10.0.4.4 -j MARK --set-mark 4
-sudo ip netns exec ny iptables -A PREROUTING -t mangle -p tcp -d 10.0.5.5 -j MARK --set-mark 5
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.1.1 -j MARK --set-mark 2
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.2.2 -j MARK --set-mark 3
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.3.3 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.4.4 -j MARK --set-mark 4
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.5.5 -j MARK --set-mark 5
