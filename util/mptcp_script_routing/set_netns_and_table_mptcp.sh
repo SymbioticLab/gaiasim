523d350b1dcf9e6383c8954ed07d4943b9a15196
jimmyyou <jieyou@umich.edu>
2018-01-23 17:04:19 -0500

    Debug ++

--- util/mptcp_script_routing/set_netns_and_table_mptcp.sh
+++ util/mptcp_script_routing/set_netns_and_table_mptcp.sh
@@ -44,7 +44,7 @@ sudo ip netns exec $net_name ifconfig vgw1 10.1.$host_id.254/24
 sudo ip netns exec $net_name ifconfig vgw2 10.2.$host_id.254/24
 
 # Change to this
-# sudo ip route add 10.0.0.0/16 via 10.0.$host_id.254
+sudo ip route add 10.0.0.0/8 via 10.0.$host_id.254
 
 # This creates 3 different routing tables, that we use based on the source-address.
 sudo ip rule add from 10.0.$host_id.$host_id table 1
@@ -53,15 +53,15 @@ sudo ip rule add from 10.2.$host_id.$host_id table 3
 
 # Configure the two different routing tables
 #sudo ip route add 10.0.0.0/16 dev $host-eth0 scope link table 1
-sudo ip route add 10.0.0.0/16 dev $host-eth0 table 1
+sudo ip route add 10.0.0.0/24 dev $host-eth0 table 1
 sudo ip route add default via 10.0.$host_id.254 dev $host-eth0 table 1
 
 #sudo ip route add 10.1.0.0/16 dev $host-eth1 scope link table 2
-sudo ip route add 10.1.0.0/16 dev $host-eth1 table 2
+sudo ip route add 10.1.0.0/24 dev $host-eth1 table 2
 sudo ip route add default via 10.1.$host_id.254 dev $host-eth1 table 2
 
 #sudo ip route add 10.2.0.0/16 dev $host-eth2 scope link table 3
-sudo ip route add 10.2.0.0/16 dev $host-eth2 table 3
+sudo ip route add 10.2.0.0/24 dev $host-eth2 table 3
 sudo ip route add default via 10.2.$host_id.254 dev $host-eth2 table 3
 
 
