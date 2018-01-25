0fae766526581268a81785130e3f5f382d343816
jimmyyou <jieyou@umich.edu>
2018-01-25 11:10:31 -0500

    Change set_equal-delay.sh

--- util/mptcp_script_routing/set_equal-dealy.sh
+++ util/mptcp_script_routing/set_equal-dealy.sh
@@ -5,5 +5,6 @@ sudo ip netns exec $(hostname -s) tc qdisc show | grep vgw | while read line ; d
     nic_name=$(echo $line | awk '{print $5}');
     echo "setting " $handle : $nic_name
     
-    sudo ip netns exec $(hostname -s) tc qdisc change dev $nic_name handle $handle netem delay 10ms
+#    sudo ip netns exec $(hostname -s) tc qdisc change dev $nic_name handle $handle netem delay 10ms
+    sudo ip netns exec $(hostname -s) tc qdisc add dev $nic_name root netem delay 60ms
 done
