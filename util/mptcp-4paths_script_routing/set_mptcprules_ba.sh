cb6f33bdecf88550af9c66da6a4bf8b02edbed99
jimmyyou <jieyou@umich.edu>
2018-01-25 15:55:53 -0500

    Fix: new routing setup for MPTCP: 2 interfaces 3 paths

--- util/mptcp-4paths_script_routing/set_mptcprules_ba.sh
+++ util/mptcp-4paths_script_routing/set_mptcprules_ba.sh
@@ -22,24 +22,24 @@ sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.0.0.0/16 -d 10.0.5.
 #2-4 NO BA
 
 #2-5
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.2.2 -d 10.1.5.5 -j MARK --set-mark 1
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.2.2 -j MARK --set-mark 3
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.2.2 -d 10.1.5.5 -j MARK --set-mark 1
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.2.2 -j MARK --set-mark 3
 
 #3-4 NO BA
 
 #3-5
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.5.5 -j MARK --set-mark 1
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.3.3 -j MARK --set-mark 3
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.5.5 -j MARK --set-mark 1
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.3.3 -j MARK --set-mark 3
 
 #4-5
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.4.4 -d 10.1.5.5 -j MARK --set-mark 1
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.4.4 -j MARK --set-mark 3
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.4.4 -d 10.1.5.5 -j MARK --set-mark 1
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.1.5.5 -d 10.1.4.4 -j MARK --set-mark 3
 
 
 
 #Overlay 3
 
 #3-4
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.0.3.3 -d 10.1.4.4 -j MARK --set-mark 3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.0.3.3 -s 10.1.4.4 -j MARK --set-mark 2
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -s 10.0.3.3 -d 10.1.4.4 -j MARK --set-mark 3
+sudo ip netns exec ba iptables -A PREROUTING -t mangle -d 10.0.3.3 -s 10.1.4.4 -j MARK --set-mark 2
 
--- util/mptcp-4paths_script_routing/set_mptcprules_ny.sh
+++ util/mptcp-4paths_script_routing/set_mptcprules_ny.sh
@@ -12,33 +12,33 @@ sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.0.0.0/16 -d 10.0.5.
 # 1-2 NO NY
 
 # 1-3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.1.1 -j MARK --set-mark 3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.1.1 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.1.1 -j MARK --set-mark 3
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.1.1 -j MARK --set-mark 1
 
 #1-4 NO NY
 
 #2-3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.2.2 -j MARK --set-mark 2
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.2.2 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.2.2 -j MARK --set-mark 2
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.2.2 -j MARK --set-mark 1
 
 #3-4
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.4.4 -j MARK --set-mark 3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.4.4 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.4.4 -j MARK --set-mark 3
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.4.4 -j MARK --set-mark 1
 
 #3-5
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.5.5 -j MARK --set-mark 4
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.5.5 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.1.3.3 -d 10.1.5.5 -j MARK --set-mark 4
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.1.3.3 -s 10.1.5.5 -j MARK --set-mark 1
 
 #4-5 NO NY
 
 
 #Overlay 3
 #2-3
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.0.2.2 -d 10.1.3.3 -j MARK --set-mark 1
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.0.2.2 -s 10.1.3.3 -j MARK --set-mark 4
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.0.2.2 -d 10.1.3.3 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.2.2 -s 10.1.3.3 -j MARK --set-mark 4
 
 #3-4
-sudo ip netns exec la iptables -A PREROUTING -t mangle -s 10.0.3.3 -d 10.1.4.4 -j MARK --set-mark 5
-sudo ip netns exec la iptables -A PREROUTING -t mangle -d 10.0.3.3 -s 10.1.4.4 -j MARK --set-mark 1
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -s 10.0.3.3 -d 10.1.4.4 -j MARK --set-mark 5
+sudo ip netns exec ny iptables -A PREROUTING -t mangle -d 10.0.3.3 -s 10.1.4.4 -j MARK --set-mark 1
 
 
